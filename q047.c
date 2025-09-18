574b89d62242365e65d87fc4cb6a036e7002eb12
Avieate <aditverma6819@gmail.com>
2025-09-18 23:02:39 +0530

    Add files via upload

---
+++ q047.c
@@ -0,0 +1,42 @@
+/*Q47: Write a program to print the following pattern:
+*
+**
+***
+****
+*****
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*
+**
+***
+****
+*****
+
+*/
+Q50: Write a program to print the following pattern:
+*****
+ ****
+  ***
+   **
+    *
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*****
+ ****
+  ***
+   **
+    *
+
+Input 2:
+
+Output 2:
+Note: Spaces indicate indentation.
+
+*/
